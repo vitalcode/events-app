f0e1bb835627fbf17b3a52dcf80cbbc7e0e46ea6
Vitaliy Kuznetsov <vitaliy.kuznetsov@yahoo.co.uk>
2016-09-26 17:38:53 +0100

    Update readme file

---
+++ README.md
@@ -0,0 +1,2 @@
+Mobile app that helps to find what is happening in your location right now or any time in future.
+
---
+++ src/config.js
@@ -0,0 +1,11 @@
+import moment from 'moment'
+
+export const Config = {
+  //host: '172.20.10.2'
+  host: '51.254.206.174', // robot01
+  port: 9200,
+  index: 'test_index',
+  //host: 'localhost',
+  today: moment('2016-01-01T00:00:00')
+};
+
